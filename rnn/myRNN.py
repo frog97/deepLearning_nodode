9f46b5339e69ae742d1b6e2f1cb925cd0cc744d5
hh.yoon <hh.yoon@lge.com>
2024-08-14 09:01:05 +0900

    수업 내용 적용

--- rnn/myRNN.py
+++ rnn/myRNN.py
@@ -22,3 +22,24 @@ class MyRNN(nn.Module):
 
 rnn_model = MyRNN(input_size=4, hidden_size=1024, output_size=2)
 hidden = rnn_model.get_hidden()
+
+# The food is good
+
+input_tensor0 = torch.randn(1, 4) # The
+input_tensor1 = torch.randn(1, 4) # food
+input_tensor2 = torch.randn(1, 4) # is
+input_tensor3 = torch.randn(1, 4) # good
+
+
+# ouput_tensor0, hidden = rnn_model(input_tensor0, hidden)   # _ 몰라도 되는 것 들 처리
+# ouput_tensor1, hidden = rnn_model(input_tensor1, hidden)
+# ouput_tensor2, hidden = rnn_model(input_tensor2, hidden)
+# ouput_tensor3, hidden = rnn_model(input_tensor3, hidden)
+
+
+_, hidden = rnn_model.forward(input_tensor0, hidden)   # _ 몰라도 되는 것 들 처리
+_, hidden = rnn_model.forward(input_tensor1, hidden)
+_, hidden = rnn_model.forward(input_tensor2, hidden)
+ouput_tensor3, _ = rnn_model.forward(input_tensor3, hidden)
+
+print(ouput_tensor3)
